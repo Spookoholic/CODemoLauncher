2eebaab2f0493357d720f628ee69b5e79ba521c9
Spookoholic <spookoholic@mailbox.org>
2024-03-03 21:09:33 -0500

    Renamed project, solution file, and paths.

---
+++ CoDemoLauncher/Parser/CostumeParser.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoDemoLauncher.Model;
+
+namespace CoDemoLauncher.Parser
+{
+    /// <summary>
+    /// Fetches all 
+    /// </summary>
+    class CostumeParser : AbstractSectionsParser
+    {
+        /// <summary>
+        /// The list of parsed resources
+        /// </summary>
+        List<DemoSection> result = new List<DemoSection>();
+
+        // placeholders
+        int braceLevel = 0;
+        int lastCostumev5BraceLevel = 0;
+        int lastCostumev5SectionStart = 0;
+        bool inCostumev5Section = false;
+        string Costumetype = "";
+        string Costumename = "";
+
+        /// <summary>
+        /// Is called by the parser when it has fetched a new line
+        /// </summary>
+        /// <param name="line">Content of the line</param>
+        /// <param name="lineNumber">Current line number (0 index)</param>
+        public override void NewLine(string line, int lineNumber)
+        {
+            if (line.Trim().StartsWith("Costumev5"))
+            {
+                lastCostumev5SectionStart = lineNumber;
+                lastCostumev5BraceLevel = braceLevel;
+                inCostumev5Section = true;
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("Hreferencedcostume"))
+            {
+                this.Costumetype = "Hreferencedcostume";
+                this.Costumename = line.Trim().Substring(19);
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("Psubstitutecostume"))
+            {
+                this.Costumetype = "Psubstitutecostume";
+                this.Costumename = line.Trim().Substring(19);
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("Pstoredcostume"))
+            {   
+                this.Costumetype = "Pstoredcostume";
+                this.Costumename = line.Trim().Substring(15);
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("Psubstitutecostume"))
+            {
+                this.Costumetype = "Psubstitutecostume";
+                this.Costumename = line.Trim().Substring(15);
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("Pcdestructibleobjectcostume"))
+            {
+                this.Costumetype = "Pcdestructibleobjectcostume";
+                this.Costumename = line.Trim().Substring(28);
+            }
+
+            if (line.Trim().StartsWith("CostumeV5"))
+            {
+                lastCostumev5SectionStart = lineNumber;
+                lastCostumev5BraceLevel = braceLevel;
+                inCostumev5Section = true;
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("hReferencedCostume"))
+            {
+                this.Costumetype = "hReferencedCostume";
+                this.Costumename = line.Trim().Substring(19);
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("pSubstituteCostume"))
+            {
+                this.Costumetype = "pSubstituteCostume";
+                this.Costumename = line.Trim().Substring(19);
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("pStoredCostume"))
+            {
+                this.Costumetype = "pStoredCostume";
+                this.Costumename = line.Trim().Substring(15);
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("pSubstituteCostume"))
+            {
+                this.Costumetype = "pSubstituteCostume";
+                this.Costumename = line.Trim().Substring(15);
+            }
+            if (inCostumev5Section && Costumetype.Equals("") && line.Trim().StartsWith("pcDestructibleObjectCostume"))
+            {
+                this.Costumetype = "pcDestructibleObjectCostume";
+                this.Costumename = line.Trim().Substring(28) + " (Ignore This)";
+            }
+
+            if (inCostumev5Section && line.Trim().StartsWith("}") && braceLevel - 1 == lastCostumev5BraceLevel)
+            {
+                inCostumev5Section = false;
+                DemoCostume resource = new DemoCostume();
+                resource.StartLine = lastCostumev5SectionStart;
+                resource.EndLine = lineNumber;
+                resource.Name = this.Costumename;
+                resource.Type = this.Costumetype;
+                result.Add(resource);
+                Costumename = "";
+                Costumetype = "";
+            }
+            if (line.EndsWith("{")) braceLevel++;
+            if (line.EndsWith("}")) braceLevel--;
+        }
+
+        /// <summary>
+        /// Returns the list with the found resources.
+        /// </summary>
+        /// <returns></returns>
+        override public List<DemoSection> GetResult()
+        {
+            return result;
+        }
+
+    }
+}
---
+++ CoDemoLauncher/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using CoDemoLauncher.Helper;
+
+namespace CoDemoLauncher
+{
+    static class Program
+    {
+        public const string StoDemoLauncherIniGroup = "CoDemoLauncher";
+        public const string VersionIniKey = "version";
+
+        /// <summary>
+        /// Main entry point for Application
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Find the installation location of STO
+            string stoInstallLocation = GameClient.FindCoDirectory();
+
+            // Create the form, if a valid path was found
+            if (!stoInstallLocation.Equals(""))
+            {
+                ConfigurationFile.GetInstance().PutValue(StoDemoLauncherIniGroup, VersionIniKey, Application.ProductVersion);
+                SplashScreen.ShowSplashScreen();
+                CoDemoLauncherForm mainWindow = new CoDemoLauncherForm(new GameClient(stoInstallLocation));
+                SplashScreen.CloseSplashScreen();
+                Application.Run(mainWindow);
+            }
+            // Quit the application, if no valid path was found
+            else
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
